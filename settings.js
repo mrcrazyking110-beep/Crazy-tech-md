868ec38665a0778dc97a9def32a9731d01bbfc11
mrcrazyking110-beep <mrcrazyking110@gmail.com>
2025-11-13 09:22:57 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,13 +3,13 @@
 
 const settings = {
   // 🏷️ Sticker Settings
-  packname: '💎 MANI-TECH-MD 💎',
-  author: '👑 ManiMD Official 🇵🇰',
+  packname: '💎 crazy-bot💎',
+  author: '👑 Crazy tech Official 🇵🇰',
 
   // 🤖 Bot Identity
-  botName: '⚡ MANI TECH MD ⚡',
-  botOwner: '🌍 MANI MD Official 🇵🇰',   // Change to your name if needed
-  ownerNumber: '923033096103',        // Your number without + symbol
+  botName: '⚡ crazy bot⚡',
+  botOwner: '🌍 mr crazy  🇵🇰',   // Change to your name if needed
+  ownerNumber: '923163622344',        // Your number without + symbol
 
   // 🎬 APIs
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq', // For GIF commands
@@ -18,7 +18,7 @@ const settings = {
   commandMode: 'public', // Options: 'public' (everyone) | 'private' (owner only)
 
   // 📝 Meta Information
-  description: '💬 A fun, powerful & stylish WhatsApp MD bot created with ❤️ by MANI POWER.',
+  description: '💬 A fun, powerful & stylish WhatsApp MD bot created with ❤️ by mr crazy POWER.',
   version: '2.0.0',
 updateZipUrl: "https://github.com/MANI-TECH-BOT/MANI-TECH-BOT/archive/refs/heads/main.zip",
   
